7cb6cd0ed38de9bf42335c06e1667fa11d2a3eb7
Glykeria Stravodimou <strglykeria@gmail.com>
2021-04-05 16:41:47 +0100

    The articles titles appear on screen

--- interface.js
+++ interface.js
@@ -1,23 +1,41 @@
 'use strict';
 
 let url = "http://news-summary-api.herokuapp.com/guardian?apiRequestUrl=http://content.guardianapis.com/search?order-by=newest&show-fields=body&show-elements=image&page-size=20&show-fields=thumbnail"
+let articles = []
 
 function getNewsData(){
   return fetch(url)
-  .then(response => { return response.json() 
-  // .then(json => {
-  //   console.log(json.response)
-  //   })
-  })
+  .then(response => { return response.json() })
 };
 
-function renderData(news){
-  return news.response.results
+function renderNews(newsData){
+  // return news.response.results
+  let allNews = newsData.response.results
+  for(let i = 0 ; i < allNews.length ; i++){
+    let selected = {
+      webTitle: allNews[i].webTitle,
+      webUrl: allNews[i].webUrl
+    }
+    articles.push(selected)
+  }
+  console.log(articles)
+  return articles
 }
 
+function renderTitles(articles){
+  let titles = []
+  for( let i = 0; i < articles.length; i++){
+    let title = articles[i].webTitle
+    // let url = articles[i].webUrl
+    let titleHTML = `<p id=#${[i]+1}>${title}</p>`
+    titles.push(titleHTML)
+  }
+  return titles
+}
 
 getNewsData()
-.then( response => {
-  console.log(renderData(response))
-}
-)
+  .then( response => {
+  let articles = renderNews(response)
+  let titles = renderTitles(articles)
+  document.getElementById('headlines').innerHTML = titles
+  })
